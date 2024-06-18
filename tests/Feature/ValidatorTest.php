24ba4c385a3c5d6218f2c77010888d9d8b6c6d36
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-18 13:17:52 +0700

    test: testMultipleValidationRules & testValidatorValidData

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -24,6 +24,7 @@ class ValidatorTest extends TestCase
 
         $validator=Validator::make($data, $rules);
         self::assertNotNull($validator);
+
         self::assertTrue($validator->passes());
         self::assertFalse($validator->fails());
     }
@@ -75,7 +76,54 @@ class ValidatorTest extends TestCase
             Log::info($message->toJson(JSON_PRETTY_PRINT));
 
         }
+    }
 
+    public function testMultipleValidationRules(){
+        $data=[
+            "username" => "fajar",
+            "password" => "rama"
+        ];
 
+        $rules=[
+            "username" => "required|email|max:100",
+            "password" => ["required","min:10","max:20"]
+        ];
+
+        $validator=Validator::make($data, $rules);
+        self::assertNotNull($validator);
+        
+        self::assertTrue($validator->fails());
+        self::assertFalse($validator->passes());
+
+        $message=$validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidatorValidData(){
+        $data=[
+            "username" => "admin@ramafajar.com",
+            "password" => "rahasia",
+            "admin" => true,
+            "others" => "xxxxx"
+        ];
+
+        $rules=[
+            "username" => "required|email|max:100",
+            "password" => "required|min:6|max:120"
+        ];
+
+        $validator=Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        try{
+            $valid=$validator->validate();
+            Log:info(json_encode($valid, JSON_PRETTY_PRINT));
+        }catch(ValidationException $exception){
+            self::assertNotNull($exception->validator);
+            $message=$exception->validator->errors();
+            Log::info($message->toJson(JSON_PRETTY_PRINT));
+
+        }
     }
 }
