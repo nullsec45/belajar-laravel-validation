21eba1f346e37c6616906f1eebaa318320e1f40c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-30 19:00:07 +0700

    test : rule class, nested indexed array

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\{Log, App};
 use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Rules\{In, Password};
 use Illuminate\Validation\Validator as AdditionalValidator;
 use App\Rules\{Uppercase, RegistrationRule};
 
@@ -280,5 +281,100 @@ class ValidatorTest extends TestCase
         $message=$validator->getMessageBag();
 
         Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidatorRuleClasses(){
+        App::setLocale("id");
+        
+        $data=[
+            "username" => "entong",
+            "password" => "kepo"
+        ];
+
+        $rules=[
+            "username" => ["required",new In("Rama","Fajar",)],
+            "password" => ["required", Password::min(6)->letters()->numbers()->symbols()]
+        ];
+
+        $messages=[
+            "required" => ":attribute harus diisi",
+            "email" => ":attribute harus berupa email",
+            "min" => ":attribute minimal :min karakter",
+            "max" => ":attribute maximal :max karakter",
+        ];
+
+        $validator=Validator::make($data, $rules);
+        $validator->after(function(AdditionalValidator $validator){
+            $data=$validator->getData();
+            if($data["username"] == $data["password"]){
+                $validator->errors()->add("password", "Password tidak boleh sama dengan username.");
+            }
+        });
+
+        self::assertNotNull($validator);
+        
+        self::assertTrue($validator->fails());
+        self::assertFalse($validator->passes());
+
+        $message=$validator->getMessageBag();
+
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testNestedArray(){
+        $data=[
+            "name" => [
+                "first" => "Rama",
+                "last" => "Fajar"
+            ],
+            "address" => [
+                "street" => "Jalanin aja dulu",
+                "city" => "Jakarta Keras",
+                "country" => "Indonesia Tanah Air Beta"
+            ]
+        ];
+        
+        $rules=[
+            "name.first" => ["required","max:100"],
+            "name.last" => ["required","max:100"],
+            "address.street" => ["max:200"],
+            "address.city" => ["required","max:100"],
+            "address.country" => ["required","max:100"]
+        ];
+
+        $validator=Validator::make($data, $rules);
+        self::assertTrue($validator->passes());
+    }
+
+    public function testNestedIndexedArray(){
+        $data=[
+            "name" => [
+                "first" => "Rama",
+                "last" => "Fajar"
+            ],
+            "address" => [
+               [
+                "street" => "Jalanin aja dulu 1",
+                "city" => "Jakarta Keras 1",
+                "country" => "Indonesia Tanah Air Beta"
+               ],
+               [
+                "street" => "Jalanin aja dulu 2",
+                "city" => "Jakarta Keras 2",
+                "country" => "Indonesia Tanah Air Beta"
+               ]
+            ]
+        ];
+        
+        $rules=[
+            "name.first" => ["required","max:100"],
+            "name.last" => ["required","max:100"],
+            "address.*.street" => ["max:200"],
+            "address.*.city" => ["required","max:100"],
+            "address.*.country" => ["required","max:100"]
+        ];
+
+        $validator=Validator::make($data, $rules);
+        self::assertTrue($validator->passes());
     }
 }
